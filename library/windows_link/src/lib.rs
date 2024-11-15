a7b214906724082142f187c0ce3696f6a9bfca95
Dennis Duda <git@seri.tools>
2024-11-16 00:17:34 +0100

    Add more `compat_fn` machinery; NT/9x startup check
    This will be necessary for some APIs that exist on both
    platforms but with different behavior/capabilities.

--- library/windows_link/src/lib.rs
+++ library/windows_link/src/lib.rs
@@ -45,7 +45,7 @@ pub macro link($($tt:tt)*) {
 #[cfg(not(feature = "windows_raw_dylib"))]
 #[cfg(not(target_os = "cygwin"))] // Cygwin doesn't need these libs
 #[cfg_attr(target_vendor = "win7", link(name = "advapi32"))]
-#[link(name = "ntdll")]
+#[cfg_attr(not(target_family = "rust9x"), link(name = "ntdll"))]
 #[link(name = "userenv")]
 #[link(name = "ws2_32")]
 #[link(name = "dbghelp")] // required for backtrace-rs symbolization
