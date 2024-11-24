86a1edca4df375d179e50c962a861fbda332b20b
Dennis Duda <git@seri.tools>
2024-11-24 17:21:49 +0100

    Remove `GetProcessId` usage
    Instead, store the ID when spawning the process.
    
    >= XP SP1 / Vista / Server 2003
    https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getprocessid

--- library/std/src/sys/pal/windows/process.rs
+++ library/std/src/sys/pal/windows/process.rs
@@ -394,6 +394,7 @@ impl Command {
                 Process {
                     handle: Handle::from_raw_handle(pi.hProcess),
                     main_thread_handle: Handle::from_raw_handle(pi.hThread),
+                    id: pi.dwProcessId,
                 },
                 pipes,
             ))
@@ -657,6 +658,7 @@ impl From<io::Stderr> for Stdio {
 pub struct Process {
     handle: Handle,
     main_thread_handle: Handle,
+    id: u32,
 }
 
 impl Process {
@@ -675,7 +677,7 @@ impl Process {
     }
 
     pub fn id(&self) -> u32 {
-        unsafe { c::GetProcessId(self.handle.as_raw_handle()) }
+        self.id
     }
 
     pub fn main_thread_handle(&self) -> BorrowedHandle<'_> {
