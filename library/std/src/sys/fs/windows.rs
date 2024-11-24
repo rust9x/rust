2655298e69e7b343f988c18f4c44b6e7bfa99f40
Dennis Duda <git@seri.tools>
2024-11-24 23:25:06 +0100

    Fallbacks for `CreateSymbolicLinkW`, `CreateHardLinkW`, `GetFinalPathNameByHandleW`
    - `CreateSymbolicLinkW` is used in `std::fs::soft_link`. Fails on unsupported systems.
    - `CreateHardLinkW` is used in `std::fs::hard_link`. Fails on unsupported systems.
    - `GetFinalPathNameByHandleW` is used in `std::fs::canonicalize`. Systems that don't support `GetFinalPathNameByHandleW` also don't support symlinks, so the fallback uses `GetFullPathNameW` instead.

--- library/std/src/sys/fs/windows.rs
+++ library/std/src/sys/fs/windows.rs
@@ -1632,6 +1632,7 @@ fn get_path(f: impl AsRawHandle) -> io::Result<PathBuf> {
     )
 }
 
+#[cfg(not(target_family = "rust9x"))]
 pub fn canonicalize(p: &WCStr) -> io::Result<PathBuf> {
     let mut opts = OpenOptions::new();
     // No read or write permissions are necessary
@@ -1640,6 +1641,27 @@ pub fn canonicalize(p: &WCStr) -> io::Result<PathBuf> {
     opts.custom_flags(c::FILE_FLAG_BACKUP_SEMANTICS);
     let f = File::open_native(p, &opts)?;
     get_path(f.handle)
+}
+
+#[cfg(target_family = "rust9x")]
+pub fn canonicalize(p: &WCStr) -> io::Result<PathBuf> {
+    if c::GetFinalPathNameByHandleW::available().is_some() {
+        let mut opts = OpenOptions::new();
+        // No read or write permissions are necessary
+        opts.access_mode(0);
+        // This flag is so we can open directories too
+        opts.custom_flags(c::FILE_FLAG_BACKUP_SEMANTICS);
+        let f = File::open_native(p, &opts)?;
+        get_path(f.handle)
+    } else {
+        // systems that don't support GetFinalPathNameByHandleW also don't support symlinks, so we
+        // fall back to using GetFullPathName.
+        let mut file_part = ptr::null_mut();
+        fill_utf16_buf(
+            |buf, sz| unsafe { c::GetFullPathNameW(p.as_ptr(), sz, buf, &mut file_part) },
+            |buf| PathBuf::from(OsString::from_wide(buf)),
+        )
+    }
 }
 
 pub fn copy(from: &WCStr, to: &WCStr) -> io::Result<u64> {
