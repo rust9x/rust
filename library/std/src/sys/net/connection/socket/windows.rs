89afa19aa662855e4954edbb2ae8290956dcc9f8
Dennis Duda <git@seri.tools>
2024-11-29 22:21:26 +0100

    Convert winsock calls to `WSA...A`

--- library/std/src/sys/net/connection/socket/windows.rs
+++ library/std/src/sys/net/connection/socket/windows.rs
@@ -118,7 +118,7 @@ pub struct Socket(OwnedSocket);
 impl Socket {
     pub fn new(family: c_int, ty: c_int) -> io::Result<Socket> {
         let socket = unsafe {
-            c::WSASocketW(
+            c::WSASocketA(
                 family,
                 ty,
                 0,
@@ -138,7 +138,7 @@ impl Socket {
             }
 
             let socket =
-                unsafe { c::WSASocketW(family, ty, 0, ptr::null_mut(), 0, c::WSA_FLAG_OVERLAPPED) };
+                unsafe { c::WSASocketA(family, ty, 0, ptr::null_mut(), 0, c::WSA_FLAG_OVERLAPPED) };
 
             if socket == c::INVALID_SOCKET {
                 return Err(last_error());
