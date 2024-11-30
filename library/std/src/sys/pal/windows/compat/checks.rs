c7a4788224bacbff3c2ff81e25217ed51f332869
Dennis Duda <git@seri.tools>
2024-11-30 20:28:20 +0100

    Add fallback for IO internals (pipe, handle):
    - Add two fallback implementations in stdio redirection:
    
      - Add fallback when 'modern' anonymous pipe creation with async io is not available (pre-Vista)
    
      - Add fallback for when named pipes are not available (9x/ME, NT before 4.0)
    
        Since Windows 9X/ME does not support creating named pipes (only
        connecting to remote pipes created on NT), we'll have to make do with
        anonymous pipes, without overlapped I/O. In particular, this means that
        we'll have to spawn another thread in the case where both stdout and
        stderr are being piped and read from (`read2`).
    
        We also use the fallback implementation on NT before 4.0, as the `Drop`
        impl of `AsyncPipe` needs to be able to cancel I/O via `CancelIo`.
    
    - Add fallbacks for `NtReadFile` and `NtWriteFile` in `synchronous_{read, write}`
    
      These might be unsound for handles that _can_ be asynchronous on 9x/ME.
      See https://github.com/rust-lang/rust/pull/95469 for more info

--- library/std/src/sys/pal/windows/compat/checks.rs
+++ library/std/src/sys/pal/windows/compat/checks.rs
@@ -16,6 +16,23 @@ pub fn is_windows_nt() -> bool {
     true // let me know once someone ported 9x to 64bit LOL
 }
 
+#[inline(always)]
+pub fn supports_async_io() -> bool {
+    unsafe { SUPPORTS_ASYNC_IO }
+}
+
+/// Whether the new way (just opening \??\PIPE\ / \Device\NamedPipe without a file/pipe name creates
+/// an anon pipe) is supported.
+///
+/// Prior to Vista (NT 6), kernel32's `CreatePipe` would create a pipe with a unique name via
+/// `_sprintf(Buffer, "\\Device\\NamedPipe\\Win32Pipes.%08x.%08x", process_id, global_counter)`;
+///
+/// see https://github.com/rust-lang/rust/pull/142517
+#[inline(always)]
+pub fn supports_anon_pipe_autoname() -> bool {
+    unsafe { SUPPORTS_ANON_PIPE_AUTONAME }
+}
+
 pub fn init_rust9x_checks() {
     // DO NOT do anything interesting or complicated in this function! DO NOT call
     // any Rust functions or CRT functions if those functions touch any global state,
@@ -26,11 +43,18 @@ pub fn init_rust9x_checks() {
     init_mutex_kind_check();
 }
 
-static mut IS_NT: bool = true;
+static mut IS_NT: bool = false;
+static mut SUPPORTS_ASYNC_IO: bool = false;
+static mut SUPPORTS_ANON_PIPE_AUTONAME: bool = false;
 
 fn init_windows_version_check() {
     // according to old MSDN info, the high-order bit is set only on 95/98/ME.
-    unsafe { IS_NT = c::GetVersion() < 0x8000_0000 };
+    unsafe {
+        let version = c::GetVersion();
+        IS_NT = version < 0x8000_0000;
+        SUPPORTS_ASYNC_IO = IS_NT && c::CancelIo::available().is_some();
+        SUPPORTS_ANON_PIPE_AUTONAME = IS_NT && version & 0xFF >= 0x06; // Vista+/NT6+
+    };
 }
 
 #[derive(Clone, Copy, PartialEq)]
