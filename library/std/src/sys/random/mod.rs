1808d2a7184a77c9d65c10fc1953a24e205b6b81
Dennis Duda <git@seri.tools>
2024-12-01 22:12:31 +0100

    Make std::hash::RandomState not be align(8) to work around TLS issues on 95
    At least on Windows 95 RTM, the TLS variables are only aligned to a 4 byte boundary. Unclear if this affects other systems, but NT4 is fine.
    
    This changes the `hashmap_random_keys` function signature, so update the Linux
    implementation to use the new signature as well and keep cross-compiling on
    Linux working.
    
    Co-authored-by: Jookia <contact@jookia.org>

--- library/std/src/sys/random/mod.rs
+++ library/std/src/sys/random/mod.rs
@@ -124,10 +124,8 @@ cfg_select! {
     target_os = "xous",
     target_os = "vexos",
 )))]
-pub fn hashmap_random_keys() -> (u64, u64) {
+pub fn hashmap_random_keys() -> [u8; 16] {
     let mut buf = [0; 16];
     fill_bytes(&mut buf);
-    let k1 = u64::from_ne_bytes(buf[..8].try_into().unwrap());
-    let k2 = u64::from_ne_bytes(buf[8..].try_into().unwrap());
-    (k1, k2)
+    buf
 }
