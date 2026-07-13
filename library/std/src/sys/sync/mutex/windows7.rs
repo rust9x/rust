1509fbb56b9227011e146b45d7cc43ec426e20a3
Dennis Duda <git@seri.tools>
2026-07-13 23:01:41 +0200

    Implement `TryAcquireSRWLockExclusive` for Vista SRWLock-based mutex support

--- library/std/src/sys/sync/mutex/windows7.rs
+++ library/std/src/sys/sync/mutex/windows7.rs
@@ -48,7 +48,13 @@ impl Mutex {
 
     #[inline]
     pub fn try_lock(&self) -> bool {
-        unsafe { c::TryAcquireSRWLockExclusive(raw(self)) }
+        use crate::sync::atomic::{AtomicUsize, Ordering};
+
+        // This is what `TryAcquireSRWLockExclusive` does: atomically set the low bit of the lock
+        // word, acquiring the lock iff that bit was previously clear. We do it inline rather than
+        // calling the API because the latter only exists on Windows 7+.
+        let lock = unsafe { AtomicUsize::from_ptr(raw(self).cast::<usize>()) };
+        lock.fetch_or(1, Ordering::Acquire) & 1 == 0
     }
 
     #[inline]
