e2a220368ee6c70db1c81c6a4ea07e37d20c06a8
Dennis Duda <git@seri.tools>
2026-07-18 17:19:35 +0200

    bring back previous TLS destructor implementation

--- library/std/src/sys/pal/windows/c.rs
+++ library/std/src/sys/pal/windows/c.rs
@@ -358,6 +358,8 @@ cfg_select! {
 #[cfg(not(any(target_vendor = "win7", target_family = "rust9x")))]
 windows_link::link!("ws2_32.dll" "system" fn GetHostNameW(name : PWSTR, namelen : i32) -> i32);
 
+// only needed for the new fiber-based thread local cleanup code, which is not used by rust9x
+#[cfg(not(target_family = "rust9x"))]
 unsafe extern "C" {
     pub fn atexit(cb: unsafe extern "C" fn()) -> c_int;
 }
