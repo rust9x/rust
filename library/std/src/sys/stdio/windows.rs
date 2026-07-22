35bbc7a7db749a85df2e7568056db26fea42735a
Dennis Duda <git@seri.tools>
2026-07-23 00:34:00 +0200

    use builtin conversion between UTF8 and 16 for console
    Effectively undoes https://github.com/rust-lang/rust/pull/107110. Allows NT<4 to display console output correctly.

--- library/std/src/sys/stdio/windows.rs
+++ library/std/src/sys/stdio/windows.rs
@@ -194,13 +194,22 @@ fn write_valid_utf8_to_console(handle: c::HANDLE, utf8: &str) -> io::Result<usiz
     let utf8 = &utf8[..utf8.floor_char_boundary(utf16.len())];
 
     let utf16: &[u16] = unsafe {
+        cfg_select! {
+            target_family = "rust9x" => {
+                let mut len_utf16 = 0;
+                for (chr, dest) in utf8.encode_utf16().zip(utf16.iter_mut()) {
+                    dest.write(chr);
+                    len_utf16 += 1;
+                }
+                utf16[..len_utf16].assume_init_ref()
+            }
+            _ => { // target_family != "rust9x"
+
+
         // Note that this theoretically checks validity twice in the (most common) case
         // where the underlying byte sequence is valid utf-8 (given the check in `write()`).
         let result = c::MultiByteToWideChar(
             c::CP_UTF8, // CodePage
-            #[cfg(target_family = "rust9x")]
-            0, // dwFlags
-            #[cfg(not(target_family = "rust9x"))]
             c::MB_ERR_INVALID_CHARS, // dwFlags
             utf8.as_ptr(), // lpMultiByteStr
             utf8.len() as i32, // cbMultiByte
@@ -211,6 +220,10 @@ fn write_valid_utf8_to_console(handle: c::HANDLE, utf8: &str) -> io::Result<usiz
 
         // Safety: MultiByteToWideChar initializes `result` values.
         utf16[..result as usize].assume_init_ref()
+
+
+            } // target_family != "rust9x" END
+        }
     };
 
     let mut written = write_u16s(handle, utf16)?;
@@ -411,10 +424,24 @@ fn utf16_to_utf8(utf16: &[u16], utf8: &mut [u8]) -> io::Result<usize> {
         return Ok(0);
     }
 
-    #[cfg(target_family = "rust9x")]
-    let is_nt = crate::sys::compat::checks::is_windows_nt();
+    cfg_select! {
+        target_family = "rust9x" => {
+            let mut written = 0;
+            let result = 'result: {
+                for chr in char::decode_utf16(utf16.iter().cloned()) {
+                    match chr {
+                        Ok(chr) => {
+                            chr.encode_utf8(&mut utf8[written..]);
+                            written += chr.len_utf8();
+                        }
+                        Err(_) => { break 'result 0; } // invalid UTF-16 sequence
+                    }
+                }
+                written as i32
+            };
+        }
+        _ => { // target_family != "rust9x"
 
-    #[cfg(not(target_family = "rust9x"))]
     let result = unsafe {
         c::WideCharToMultiByte(
             c::CP_UTF8,              // CodePage
@@ -427,37 +454,10 @@ fn utf16_to_utf8(utf16: &[u16], utf8: &mut [u8]) -> io::Result<usize> {
             ptr::null_mut(),         // lpUsedDefaultChar
         )
     };
-    #[cfg(target_family = "rust9x")]
-    let result = {
-        let mut result = unsafe {
-            c::WideCharToMultiByte(
-                c::CP_UTF8,                                      // CodePage
-                if is_nt { c::WC_ERR_INVALID_CHARS } else { 0 }, // dwFlags
-                utf16.as_ptr(),                                  // lpWideCharStr
-                utf16.len() as i32,                              // cchWideChar
-                utf8.as_mut_ptr(),                               // lpMultiByteStr
-                utf8.len() as i32,                               // cbMultiByte
-                ptr::null(),                                     // lpDefaultChar
-                ptr::null_mut(),                                 // lpUsedDefaultChar
-            )
-        };
 
-        if result == 0 && unsafe { c::GetLastError() } == c::ERROR_INVALID_FLAGS && is_nt {
-            result = unsafe {
-                c::WideCharToMultiByte(
-                    c::CP_UTF8,         // CodePage
-                    0,                  // dwFlags (0 for pre-Vista compatibility)
-                    utf16.as_ptr(),     // lpWideCharStr
-                    utf16.len() as i32, // cchWideChar
-                    utf8.as_mut_ptr(),  // lpMultiByteStr
-                    utf8.len() as i32,  // cbMultiByte
-                    ptr::null(),        // lpDefaultChar
-                    ptr::null_mut(),    // lpUsedDefaultChar
-                )
-            };
         }
-        result
-    };
+    }
+
     if result == 0 {
         // We can't really do any better than forget all data and return an error.
         Err(io::const_error!(
