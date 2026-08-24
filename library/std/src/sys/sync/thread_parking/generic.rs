4f2e3e40f5406938e9d7b289cc81e3d11af6fd39
Dennis Duda <git@seri.tools>
2026-08-25 00:10:46 +0200

    update generic thread parker based on pthread parker

--- library/std/src/sys/sync/thread_parking/generic.rs
+++ library/std/src/sys/sync/thread_parking/generic.rs
@@ -1,9 +1,9 @@
-//! Parker implementation based on a Mutex and Condvar.
+//! Thread parking without `futex` using the `pthread` synchronization primitives.
 
 use crate::pin::Pin;
-use crate::sync::atomic::AtomicUsize;
-use crate::sync::atomic::Ordering::SeqCst;
-use crate::sync::{Condvar, Mutex};
+use crate::sync::atomic::Ordering::{Acquire, Relaxed, Release};
+use crate::sync::atomic::{Atomic, AtomicUsize};
+use crate::sys::sync::{Condvar, Mutex};
 use crate::time::Duration;
 
 const EMPTY: usize = 0;
@@ -11,33 +11,46 @@ const PARKED: usize = 1;
 const NOTIFIED: usize = 2;
 
 pub struct Parker {
-    state: AtomicUsize,
-    lock: Mutex<()>,
+    state: Atomic<usize>,
+    lock: Mutex,
     cvar: Condvar,
 }
 
 impl Parker {
-    /// Construct the generic parker. The UNIX parker implementation
-    /// requires this to happen in-place.
+    /// Constructs the UNIX parker in-place.
+    ///
+    /// # Safety
+    /// The constructed parker must never be moved.
     pub unsafe fn new_in_place(parker: *mut Parker) {
         parker.write(Parker {
             state: AtomicUsize::new(EMPTY),
-            lock: Mutex::new(()),
+            lock: Mutex::new(),
             cvar: Condvar::new(),
         });
     }
 
-    // This implementation doesn't require `unsafe` and `Pin`, but other implementations do.
+    fn lock(self: Pin<&Self>) -> Pin<&Mutex> {
+        unsafe { self.map_unchecked(|p| &p.lock) }
+    }
+
+    fn cvar(self: Pin<&Self>) -> Pin<&Condvar> {
+        unsafe { self.map_unchecked(|p| &p.cvar) }
+    }
+
+    // This implementation doesn't require `unsafe`, but other implementations
+    // may assume this is only called by the thread that owns the Parker.
+    //
+    // For memory ordering, see futex.rs
     pub unsafe fn park(self: Pin<&Self>) {
         // If we were previously notified then we consume this notification and
         // return quickly.
-        if self.state.compare_exchange(NOTIFIED, EMPTY, SeqCst, SeqCst).is_ok() {
+        if self.state.compare_exchange(NOTIFIED, EMPTY, Acquire, Relaxed).is_ok() {
             return;
         }
 
         // Otherwise we need to coordinate going to sleep
-        let mut m = self.lock.lock().unwrap();
-        match self.state.compare_exchange(EMPTY, PARKED, SeqCst, SeqCst) {
+        self.lock().lock();
+        match self.state.compare_exchange(EMPTY, PARKED, Relaxed, Relaxed) {
             Ok(_) => {}
             Err(NOTIFIED) => {
                 // We must read here, even though we know it will be `NOTIFIED`.
@@ -46,54 +59,76 @@ impl Parker {
                 // acquire operation that synchronizes with that `unpark` to observe
                 // any writes it made before the call to unpark. To do that we must
                 // read from the write it made to `state`.
-                let old = self.state.swap(EMPTY, SeqCst);
+                let old = self.state.swap(EMPTY, Acquire);
+
+                self.lock().unlock();
+
                 assert_eq!(old, NOTIFIED, "park state changed unexpectedly");
                 return;
             } // should consume this notification, so prohibit spurious wakeups in next park.
-            Err(_) => panic!("inconsistent park state"),
-        }
-        loop {
-            m = self.cvar.wait(m).unwrap();
-            match self.state.compare_exchange(NOTIFIED, EMPTY, SeqCst, SeqCst) {
-                Ok(_) => return, // got a notification
-                Err(_) => {}     // spurious wakeup, go back to sleep
+            Err(_) => {
+                self.lock().unlock();
+
+                panic!("inconsistent park state")
             }
         }
+
+        loop {
+            self.cvar().wait(self.lock().get_ref());
+
+            match self.state.compare_exchange(NOTIFIED, EMPTY, Acquire, Relaxed) {
+                Ok(_) => break, // got a notification
+                Err(_) => {}    // spurious wakeup, go back to sleep
+            }
+        }
+
+        self.lock().unlock();
     }
 
-    // This implementation doesn't require `unsafe` and `Pin`, but other implementations do.
+    // This implementation doesn't require `unsafe`, but other implementations
+    // may assume this is only called by the thread that owns the Parker. Use
+    // `Pin` to guarantee a stable address for the mutex and condition variable.
     pub unsafe fn park_timeout(self: Pin<&Self>, dur: Duration) {
         // Like `park` above we have a fast path for an already-notified thread, and
         // afterwards we start coordinating for a sleep.
         // return quickly.
-        if self.state.compare_exchange(NOTIFIED, EMPTY, SeqCst, SeqCst).is_ok() {
+        if self.state.compare_exchange(NOTIFIED, EMPTY, Acquire, Relaxed).is_ok() {
             return;
         }
-        let m = self.lock.lock().unwrap();
-        match self.state.compare_exchange(EMPTY, PARKED, SeqCst, SeqCst) {
+
+        self.lock().lock();
+        match self.state.compare_exchange(EMPTY, PARKED, Relaxed, Relaxed) {
             Ok(_) => {}
             Err(NOTIFIED) => {
                 // We must read again here, see `park`.
-                let old = self.state.swap(EMPTY, SeqCst);
+                let old = self.state.swap(EMPTY, Acquire);
+                self.lock().unlock();
+
                 assert_eq!(old, NOTIFIED, "park state changed unexpectedly");
                 return;
             } // should consume this notification, so prohibit spurious wakeups in next park.
-            Err(_) => panic!("inconsistent park_timeout state"),
+            Err(_) => {
+                self.lock().unlock();
+                panic!("inconsistent park_timeout state")
+            }
         }
 
         // Wait with a timeout, and if we spuriously wake up or otherwise wake up
         // from a notification we just want to unconditionally set the state back to
         // empty, either consuming a notification or un-flagging ourselves as
         // parked.
-        let (_m, _result) = self.cvar.wait_timeout(m, dur).unwrap();
-        match self.state.swap(EMPTY, SeqCst) {
-            NOTIFIED => {} // got a notification, hurray!
-            PARKED => {}   // no notification, alas
-            n => panic!("inconsistent park_timeout state: {n}"),
+        self.cvar().wait_timeout(self.lock().get_ref(), dur);
+
+        match self.state.swap(EMPTY, Acquire) {
+            NOTIFIED => self.lock().unlock(), // got a notification, hurray!
+            PARKED => self.lock().unlock(),   // no notification, alas
+            n => {
+                self.lock().unlock();
+                panic!("inconsistent park_timeout state: {n}")
+            }
         }
     }
 
-    // This implementation doesn't require `Pin`, but other implementations do.
     pub fn unpark(self: Pin<&Self>) {
         // To ensure the unparked thread will observe any writes we made
         // before this call, we must perform a release operation that `park`
@@ -101,7 +136,7 @@ impl Parker {
         // `state` is already `NOTIFIED`. That is why this must be a swap
         // rather than a compare-and-swap that returns if it reads `NOTIFIED`
         // on failure.
-        match self.state.swap(NOTIFIED, SeqCst) {
+        match self.state.swap(NOTIFIED, Release) {
             EMPTY => return,    // no one was waiting
             NOTIFIED => return, // already unparked
             PARKED => {}        // gotta go wake someone up
@@ -119,7 +154,10 @@ impl Parker {
         // Releasing `lock` before the call to `notify_one` means that when the
         // parked thread wakes it doesn't get woken only to have to wait for us
         // to release `lock`.
-        drop(self.lock.lock().unwrap());
-        self.cvar.notify_one()
+        unsafe {
+            self.lock().lock();
+            self.lock().unlock();
+            self.cvar().notify_one();
+        }
     }
 }
